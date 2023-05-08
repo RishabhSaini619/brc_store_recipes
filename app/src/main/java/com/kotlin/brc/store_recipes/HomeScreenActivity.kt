e9fc9e0a7e4e4460917d3f2f68a8769aa07dc05f
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-05-08 22:22:31 +0530

    item main category layout

--- app/src/main/java/com/kotlin/brc/store_recipes/HomeScreenActivity.kt
+++ app/src/main/java/com/kotlin/brc/store_recipes/HomeScreenActivity.kt
@@ -3,7 +3,7 @@ package com.kotlin.brc.store_recipes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomeScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen_home)
