5f656cc32aade4cdd2a05177784c5306478f28d3
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-05-09 18:09:50 +0530

    base activity

---
+++ app/src/main/java/com/kotlin/brc/store_recipes/BaseActivity.kt
@@ -0,0 +1,26 @@
+package com.kotlin.brc.store_recipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.CoroutineContext
+
+class BaseActivity : AppCompatActivity() , CoroutineScope{
+
+    private lateinit var job: Job
+
+   override val  coroutineContext:CoroutineContext
+        get() = job +Dispatchers.Main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
--- app/src/main/java/com/kotlin/brc/store_recipes/SplashScreenActivity.kt
+++ app/src/main/java/com/kotlin/brc/store_recipes/SplashScreenActivity.kt
@@ -3,9 +3,23 @@ package com.kotlin.brc.store_recipes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.Toast
+import com.kotlin.brc.store_recipes.database.RecipeDatabase
+import com.kotlin.brc.store_recipes.entities.Category
+import com.kotlin.brc.store_recipes.entities.Meals
+import com.kotlin.brc.store_recipes.entities.MealsItems
+import com.kotlin.brc.store_recipes.interfaces.GetDataService
+import com.kotlin.brc.store_recipes.retofitclient.RetrofitClientInstance
+import kotlinx.coroutines.launch
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -32,4 +46,5 @@ class SplashScreenActivity : AppCompatActivity() {
         finish()
     }
     }
+
 }
